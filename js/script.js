54fe264fbd362e6f5b972b3025b5ee7b4671c726
初コミット <miyawakikunito@miywakikunito-no-MacBook-Pro.local>
2017-10-20 11:22:31 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function show_block(){
+    if(document.getElementById("hamburger-menu").style.display == "block"){
+       document.getElementById("hamburger-menu").style.display = "none";
+    }else{
+       document.getElementById("hamburger-menu").style.display = "block";
+    }
+ }
